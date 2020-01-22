4386eb56e1f0e111d8cc6b65b1467be2cbfcd7df
koke5977 <koke5977@outlook.jp>
2020-01-23 02:40:07 +0900

    used lfs

--- makedb.py
+++ makedb.py
@@ -1,27 +1,3 @@
-import os, glob, pickle
-import tfidf
-
-y = []
-x = []
-
-def read_files(path, label):
-    print("read_files=", path)
-    files = glob.glob(path + "/*.txt")
-    for f in files:
-        if os.path.basename(f) == 'LICENSE.txt': continue
-        tfidf.add_file(f)
-        y.append(label)
-
-read_files('text/sports-watch', 0)
-read_files('text/it-life-hack', 1)
-read_files('text/movie-enter', 2)
-read_files('text/dokujo-tsushin', 3)
-
-x = tfidf.calc_files()
-
-pickle.dump([y, x], open('text/genre.pickle', 'wb'))
-tfidf.save_dic('text/genre-tdidf.dic')
-print('ok')
-
-print(len(x))
-print(len(y))
+version https://git-lfs.github.com/spec/v1
+oid sha256:870b9a46d417e2fee3090888b38cf14baceb709a3a5875b5d2a39ff48baa4af2
+size 607
